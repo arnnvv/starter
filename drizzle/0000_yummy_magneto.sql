9be1d3903a162e4abc6000d7c68c3e070ffdc9b1
arnnvv <arnav.900@protonmail.com>
2025-01-20 00:46:48 +0530

    db_migrations_btr

---
+++ drizzle/0000_yummy_magneto.sql
@@ -0,0 +1,54 @@
+CREATE TABLE IF NOT EXISTS "starter_email_verification_request" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "user_id" integer NOT NULL,
+  "email" text NOT NULL,
+  "code" text NOT NULL,
+  "expires_at" timestamp with time zone NOT NULL
+);
+-- statement-breakpoint
+
+CREATE TABLE IF NOT EXISTS "starter_sessions" (
+  "id" text PRIMARY KEY NOT NULL,
+  "user_id" integer NOT NULL,
+  "expires_at" timestamp with time zone NOT NULL
+);
+-- statement-breakpoint
+
+CREATE TABLE IF NOT EXISTS "starter_users" (
+  "id" serial PRIMARY KEY NOT NULL,
+  "username" varchar NOT NULL,
+  "email" varchar(255) NOT NULL,
+  "password_hash" varchar NOT NULL,
+  "verified" boolean DEFAULT false NOT NULL,
+  "picture" text,
+  CONSTRAINT "starter_users_username_unique" UNIQUE ("username"),
+  CONSTRAINT "starter_users_email_unique" UNIQUE ("email")
+);
+-- statement-breakpoint
+
+DO $$
+BEGIN
+  ALTER TABLE "starter_email_verification_request"
+  ADD CONSTRAINT "starter_email_verification_request_user_id_starter_users_id_fk"
+  FOREIGN KEY ("user_id") REFERENCES "public"."starter_users"("id")
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+EXCEPTION
+  WHEN duplicate_object THEN
+    NULL;
+END
+$$;
+-- statement-breakpoint
+
+DO $$
+BEGIN
+  ALTER TABLE "starter_sessions"
+  ADD CONSTRAINT "starter_sessions_user_id_starter_users_id_fk"
+  FOREIGN KEY ("user_id") REFERENCES "public"."starter_users"("id")
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+EXCEPTION
+  WHEN duplicate_object THEN
+  NULL;
+END
+$$;
